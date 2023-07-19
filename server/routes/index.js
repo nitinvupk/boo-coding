d9d22699808b64dc231861285f746bc8ea12be84
nitinvupk <nitinvupk83@gmail.com>
2023-07-19 12:32:18 +0530

    quick fixes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,13 @@
 const ProfileRouter = require('./profile.routes');
 const PostRouter = require('./post.routes');
+const CommentRouter = require('./comment.routes');
 const LikeRouter = require('./like.routes');
 
 const Router = require('express').Router();
 
 Router.use('/profile', ProfileRouter);
 Router.use('/post', PostRouter);
+Router.use('/comment', CommentRouter);
 Router.use('/like', LikeRouter);
 
 module.exports = Router;
